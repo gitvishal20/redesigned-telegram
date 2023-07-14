729a10d97e8d26b61f9960afcb0faf03746d3a00
gitvishal20 <139530715+gitvishal20@users.noreply.github.com>
2023-07-14 16:17:16 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+*/
+document.getElementById("myH").innerHTML = "JavaScript Comments";
+document.getElementById("myP").innerHTML = "JavaScript Comments ";
+</script>
